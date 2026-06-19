6cef1104e5d7ec5233df4f2c72616c93d6500705
Aldinokhalifah <khalifahaldino@gmail.com>
2026-06-19 14:21:29 +0700

    Mengganti metode http

--- src/main/java/com/example/event_ticketing/controllers/OrderController.java
+++ src/main/java/com/example/event_ticketing/controllers/OrderController.java
@@ -6,6 +6,9 @@ import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +21,6 @@ import com.example.event_ticketing.models.User;
 import com.example.event_ticketing.services.OrderService;
 
 import jakarta.validation.Valid;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/api/orders")
@@ -72,7 +71,7 @@ public class OrderController {
         );
     }
 
-    @PutMapping("/{id}/pay")
+    @PatchMapping("/{id}/pay")
     public ResponseEntity<Object> payOrder(@PathVariable UUID id, @AuthenticationPrincipal User currentUser) {
         OrderResponse orderPaid = orderService.payOrder(id, currentUser);
 
@@ -85,7 +84,7 @@ public class OrderController {
         );
     }
 
-    @PutMapping("/{id}/cancel")
+    @PatchMapping("/{id}/cancel")
     public ResponseEntity<Object> cancelOrder(@PathVariable UUID id, @AuthenticationPrincipal User currentUser) {
         OrderResponse orderCancelled = orderService.cancelOrder(id, currentUser);
 
